3e41b12e5c0485a92bb49298c197cd4f151e42c7
Nikhil Woodruff <nikhil.woodruff@outlook.com>
2024-10-21 17:01:38 +0100

    Add private school student count calibration

--- policyengine_uk_data/datasets/frs/enhanced_frs.py
+++ policyengine_uk_data/datasets/frs/enhanced_frs.py
@@ -25,6 +25,10 @@ class EnhancedFRS(Dataset):
 
         impute_cg_to_dataset(self)
 
+        self.save_dataset(data)
+
+        self.add_random_variables(data)
+
         # Reweighting
 
         data = self.load_dataset()
@@ -35,6 +39,29 @@ class EnhancedFRS(Dataset):
                 original_weights, loss_matrix, targets_array
             )
             data["household_weight"][str(year)] = new_weights
+
+        self.save_dataset(data)
+
+    def add_random_variables(self, data: dict):
+        from policyengine_uk import Microsimulation
+
+        simulation = Microsimulation(dataset=self)
+        RANDOM_VARIABLES = [
+            "would_evade_tv_licence_fee",
+            "would_claim_pc",
+            "would_claim_uc",
+            "would_claim_child_benefit",
+            "main_residential_property_purchased_is_first_home",
+            "household_owns_tv",
+            "is_higher_earner",
+            "attends_private_school",
+        ]
+        INPUT_PERIODS = list(range(self.time_period, self.time_period + 10))
+        for variable in RANDOM_VARIABLES:
+            simulation.get_holder(variable).delete_arrays()
+        for variable in RANDOM_VARIABLES:
+            value = simulation.calculate(variable, self.time_period).values
+            data[variable] = {period: value for period in INPUT_PERIODS}
 
         self.save_dataset(data)
 
@@ -57,7 +84,7 @@ class EnhancedFRS_2022_23(EnhancedFRS):
     input_frs = ExtendedFRS_2022_23
     time_period = 2022
     end_year = 2028
-    url = "release://PolicyEngine/ukda/1.5.0/enhanced_frs_2022_23.h5"
+    url = "release://PolicyEngine/ukda/1.6.0/enhanced_frs_2022_23.h5"
 
 
 if __name__ == "__main__":
