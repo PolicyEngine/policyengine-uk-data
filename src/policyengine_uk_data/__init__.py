4b7a88e4e0bf1f14618a36403bffee06c6fc6574
Nikhil Woodruff <nikhil.woodruff@outlook.com>
2025-04-15 09:49:01 +0100

    Finish rename

--- src/policyengine_uk_data/__init__.py
+++ src/policyengine_uk_data/__init__.py
@@ -3,5 +3,5 @@ from pathlib import Path
 data_folder = Path(__file__).parent.parent.parent / "data"
 
 def main() -> None:
-    """Print a hello message from the policyengine-uk-data-v2 package."""
-    print("Hello from policyengine-uk-data-v2!")
+    """Print a hello message from the policyengine-uk-data package."""
+    print("Hello from policyengine-uk-data!")
