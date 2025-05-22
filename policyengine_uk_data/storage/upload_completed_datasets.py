50ff3fc87cedf6e77c64a27e4d9805129da72f00
Nikhil Woodruff <nikhil.woodruff@outlook.com>
2025-05-22 10:27:53 +0100

    Update data in Google Cloud Bucket Fixes #102

--- policyengine_uk_data/storage/upload_completed_datasets.py
+++ policyengine_uk_data/storage/upload_completed_datasets.py
@@ -1,9 +1,16 @@
 from policyengine_uk_data.datasets import EnhancedFRS_2022_23, FRS_2022_23
 from policyengine_uk_data.storage import STORAGE_FOLDER
 from policyengine_uk_data.utils.huggingface import upload
+from google.cloud import storage
+import google.auth
 
 
 def upload_datasets():
+    credentials, project_id = google.auth.default()
+    storage_client = storage.Client(
+        credentials=credentials, project=project_id
+    )
+    bucket = storage_client.bucket("policyengine-uk-data-private")
     for dataset in [FRS_2022_23, EnhancedFRS_2022_23]:
         dataset = dataset()
         if not dataset.exists:
@@ -16,6 +23,12 @@ def upload_datasets():
             "policyengine/policyengine-uk-data",
             dataset.file_path.name,
         )
+        blob = dataset.file_path.name
+        blob = bucket.blob(blob)
+        blob.upload_from_filename(dataset.file_path)
+        print(
+            f"Uploaded {dataset.file_path.name} to GCS bucket policyengine-uk-data-private."
+        )
 
     # Constituency weights:
 
@@ -25,12 +38,28 @@ def upload_datasets():
         "parliamentary_constituency_weights.h5",
     )
 
+    blob = "parliamentary_constituency_weights.h5"
+    blob = bucket.blob(blob)
+    blob.upload_from_filename(
+        STORAGE_FOLDER / "parliamentary_constituency_weights.h5"
+    )
+    print(
+        f"Uploaded parliamentary_constituency_weights.h5 to GCS bucket policyengine-uk-data-private."
+    )
+
     # Local authority weights:
 
     upload(
         STORAGE_FOLDER / "local_authority_weights.h5",
         "policyengine/policyengine-uk-data",
         "local_authority_weights.h5",
+    )
+
+    blob = "local_authority_weights.h5"
+    blob = bucket.blob(blob)
+    blob.upload_from_filename(STORAGE_FOLDER / "local_authority_weights.h5")
+    print(
+        f"Uploaded local_authority_weights.h5 to GCS bucket policyengine-uk-data-private."
     )
 
 
