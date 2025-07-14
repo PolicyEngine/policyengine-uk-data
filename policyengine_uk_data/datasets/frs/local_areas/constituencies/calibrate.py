f1ee161254bd8d823e7419f9dac51fe2f5ab4f0b
Nikhil Woodruff <nikhil.woodruff@outlook.com>
2025-07-14 15:37:06 +0100

    Revert!

--- policyengine_uk_data/datasets/frs/local_areas/constituencies/calibrate.py
+++ policyengine_uk_data/datasets/frs/local_areas/constituencies/calibrate.py
@@ -183,11 +183,11 @@ def calibrate(
                 with h5py.File(
                     STORAGE_FOLDER / "enhanced_frs_2023_24.h5", "r+"
                 ) as f:
-                    if "household_weight/2025" in f:
-                        del f["household_weight/2025"]
+                    if "household_weight/2023" in f:
+                        del f["household_weight/2023"]
                     f.create_dataset(
-                        "household_weight/2025",
-                        data=final_weights.sum(axis=0),
+                        "household_weight/2023",
+                        data=final_weights.sum(axis=0) / 1.021,
                     )
         l.backward()
         optimizer.step()
