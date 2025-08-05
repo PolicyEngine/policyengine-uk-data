6429d9f552ab2e5f1ba31d1f6c8f3c4295130344
Nikhil Woodruff <nikhil.woodruff@outlook.com>
2025-08-05 19:08:38 +0100

    Add download function

--- policyengine_uk_data/__init__.py
+++ policyengine_uk_data/__init__.py
@@ -1,1 +1,8 @@
 from .datasets import *
+from .storage.download_private_prerequisites import (
+    download_prerequisites,
+    check_prerequisites,
+)
+
+# Check prerequisites on import and warn if missing
+check_prerequisites()
