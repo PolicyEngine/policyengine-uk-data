96146311adb1a698a2fac121254abf8747473587
Nikhil Woodruff <nikhil.woodruff@outlook.com>
2025-10-02 15:27:27 +0100

    Format

--- policyengine_uk_data/tests/test_aggregates.py
+++ policyengine_uk_data/tests/test_aggregates.py
@@ -2,7 +2,7 @@ import pytest
 
 AGGREGATES = {
     "nhs_spending": 200e9,
-    #"dfe_education_spending": 70e9,
+    # "dfe_education_spending": 70e9,
     "rail_subsidy_spending": 12e9,
     "bus_subsidy_spending": 2.5e9,
 }
